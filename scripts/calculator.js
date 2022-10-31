832e6a64fff6fba5ff8ad4fa837a4b0a65c71575
sagihgithub <116554366+sagihgithub@users.noreply.github.com>
2022-10-31 20:45:38 +0200

    Add files via upload

---
+++ scripts/calculator.js
@@ -0,0 +1,21 @@
+﻿function openLgn() {
+    //alert('login');
+    document.getElementById("dv_ovrly").style.display = "flex";
+    //return false;
+}
+
+function clsLgn() {
+    //alert('login');
+    document.getElementById("dv_ovrly").style.display = "none";
+    //return false;
+}
+
+function gnrtRow() {
+    alert('generate row');
+    return false;
+}
+
+function rstCalc() {
+    alert('reset');
+    //return false;
+}
---
+++ scripts/overlay.js
@@ -0,0 +1,14 @@
+﻿function rstPsswrd() {
+    alert('reset password');
+    //return false;
+}
+
+function login() {
+    alert('login');
+    return false;
+}
+
+function signin() {
+    alert('sign in');
+    return false;
+}
