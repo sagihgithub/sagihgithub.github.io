860ec1a4a49211e914473f5d58548bcac5a493eb
sagihgithub <116554366+sagihgithub@users.noreply.github.com>
2023-12-30 17:55:08 +0200

    Add files via upload

---
+++ scripts/farm.js
@@ -0,0 +1,61 @@
+﻿//set main menu background on scroll
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".fixed-top");
+        $nav.toggleClass('scrolled top-shadow', $(this).scrollTop() > $nav.height());
+    });
+});
+//set main menu background on scroll
+
+//fade-in
+function reveal() {
+    var reveals = document.querySelectorAll(".fade-in-bottom, .fade-in-top");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 100;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+//fade-in
+
+//FAQ
+//const items = document.querySelectorAll(".accordion button");
+
+//function toggleAccordion() {    
+//    const itemToggle = this.getAttribute('aria-expanded');
+    
+//    for (i = 0; i < items.length; i++) {
+//        items[i].setAttribute('aria-expanded', 'false');
+//    }
+
+//    if (itemToggle == 'false') {
+//        this.setAttribute('aria-expanded', 'true');
+//    }
+//}
+
+//items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+function toggleAccordion(elm) {
+    let items = document.querySelectorAll(".accordion button");
+    let itemToggle = elm.getAttribute('aria-expanded');
+    
+    for (i = 0; i < items.length; i++) {
+        items[i].setAttribute('aria-expanded', 'false');
+    }
+
+    if (itemToggle == 'false') {
+        elm.setAttribute('aria-expanded', 'true');
+    }
+}
+
+
+//FAQ
