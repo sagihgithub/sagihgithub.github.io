da718dfe7ca5eb9482c255fbafa363254c24e5af
sagihgithub <116554366+sagihgithub@users.noreply.github.com>
2024-01-27 18:37:10 +0200

    Update home.js

--- farm/scripts/home.js
+++ farm/scripts/home.js
@@ -8,26 +8,6 @@ $(function () {
 });
 //set main menu background on scroll
 
-//fade-in
-function reveal() {
-    var reveals = document.querySelectorAll(".fade-in-bottom, .fade-in-top");
-
-    for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 100;
-
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
-        } else {
-            reveals[i].classList.remove("active");
-        }
-    }
-}
-
-window.addEventListener("scroll", reveal);
-//fade-in
-
 //FAQ
 function toggleAccordion(elm) {
     let items = document.querySelectorAll(".accordion button");
