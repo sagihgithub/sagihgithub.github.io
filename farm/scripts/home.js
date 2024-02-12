e37f54e43add114e2bc5c93a4a47a2a57281b04d
sagihgithub <116554366+sagihgithub@users.noreply.github.com>
2024-02-12 10:39:29 +0200

    Update home.js

--- farm/scripts/home.js
+++ farm/scripts/home.js
@@ -1,15 +1,3 @@
-$("#main-menu").addClass("fixed-top mb-gradient-background");
-
-//set main menu background on scroll
-$(function () {
-    $(document).scroll(function () {
-        //var $nav = $(".fixed-top");
-        var $nav = $("#main-menu");
-        //$nav.toggleClass('scrolled top-shadow', $(this).scrollTop() > $nav.height());
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
-});
-//set main menu background on scroll
 
 //FAQ
 function toggleAccordion(elm) {
